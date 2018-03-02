4c374cfc2694b1b534321b4e8139524b3033c101
ncasale <ncasale@ncsu.edu>
2018-03-02 18:58:22 -0500

    fix getEntropy(.), remove old code

--- code/decision tree/getEntropy.m
+++ code/decision tree/getEntropy.m
@@ -13,22 +13,18 @@ function to get entropy of a set
 function ent = getEntropy(set)
 
 	ent = 0;
-
-	% is there a better way to find constituent classes?
-	% O(n)
-	%classes = unique(set{2});
 	
 	% find unique ~and~ count instances in O(n) (saves n computations)
 	classes = [set{2}(1) 1];
 	for i = 2:length(set{2})
 		
-		ind = find(classes == set{2}(i), 1);
+		ind = find(classes(:,1) == set{2}(i), 1);
 		if isempty(ind)
 			% new class encountered
 			classes = [classes; set{2}(i) 1];
 		else
 			% increment counter
-			classes(ind,2) = classes(ind, 2) + 1;
+			classes(ind, 2) = classes(ind, 2) + 1;
 		end
 		
 	end
@@ -38,12 +34,6 @@ function ent = getEntropy(set)
 		% proportion of the # of elements in class x 
 		% to the number of elements in set
 		probX = classes(i,2)/length(set{2});
-		
-		%class = classes(i);
-		% likewise, this is O(n)
-		%inds = find(set{2} == class);
-		% O(n) or better?
-		%probX = numel(inds)/numel(set{2});
 		
 		% entropy
 		ent = ent - probX*log2(probX);
