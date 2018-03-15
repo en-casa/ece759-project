820f189171549d927076a33fd3913b67dc508523
ncasale <ncasale@ncsu.edu>
2018-03-15 11:26:30 -0400

    update report, reorg folders

--- code/decisionTree/main_dt_MNIST.m
+++ code/decisionTree/main_dt_MNIST.m
@@ -14,8 +14,7 @@ features are generated using principal compenent analysis
 %}
 
 clear; close all;
-addpath('utility', 'MNIST', 'MNIST/data', 'MNIST/loadMNIST', ...
-	'decision tree');
+addpath('utility', 'MNIST', 'MNIST/data', 'MNIST/loadMNIST');
 
 fprintf('begin MNIST decision tree script\n');
 
@@ -27,12 +26,12 @@ N_te = 35e3; % test samples
 usePCA = false;
 if usePCA
 	numFeatures = 30;
-else
+else % use LDA
 	numFeatures = 10;
 end
 
 % for decision tree
-minLeaf = 1; % to prevent overfitting
+minLeaf = 2; % to prevent overfitting
 
 seed = 152039828;
 rng(seed); % for reproducibility
@@ -41,7 +40,7 @@ rng(seed); % for reproducibility
 % MNIST contains 70k examples
 [train, test] = loadMNIST(N_tr);
 
-%% dimensionality reduction / feature generation (~3 minutes)
+%% dimensionality reduction / feature generation
 % via prinicpal component analysis (pca) (svd)
 st = cputime;
 
@@ -53,14 +52,14 @@ end
 
 fprintf('Features Generated in %4.2f minutes\n', (cputime - st)/60);
 
-%% train (~50 minutes)
+%% train
 st = cputime;
 
 tree = trainDecisionTree({train{2:3}}, minLeaf);
 
 fprintf('Trained in %4.2f minutes\n',(cputime - st)/60);
 
-%% test (~2 minutes)
+%% test
 st = cputime;
 
 if usePCA
@@ -76,7 +75,7 @@ fprintf('Tested in %4.2f minutes\n', (cputime - st)/60);
 errors = nnz(test{2}(:,1) ~= test{2}(:,2));
 errorRate = (errors/N_te)*100;
 
-filename = sprintf('tree%2.0f.mat', errorRate);
+filename = sprintf('mnist_tree%2.0f_%d.mat', errorRate, minLeaf);
 save(filename, 'tree');
 %filename = sprintf('UV%2.0f.mat', errorRate);
 %save(filename, 'U', 'V', '-v7.3');
@@ -86,7 +85,7 @@ fprintf('numFeatures: %d, minLeaf: %d, error rate: %2.2f\n', ...
 
 %% Test Results
 %{
- 					  ,  *  *   *   &   &    ,    ,    ,    ,        ,
+                 ,  *  *   *   &   &    ,    ,    ,    ,        ,
 numFeatures   : 30 30 60 200 200 200   30  100  100   20 10 (lda)
 minLeaf       :  1  1  2   2   3   4    4    4    1    1        1
 Error Rate    : 22 26 26  26  23  24 22.6 23.6 23.5 22.9    18.42
