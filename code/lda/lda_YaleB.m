a2e2d4007cee8ecd1f1eea2daaec64fe3954b2e9
Kudiyar Orazymbetov <kudi.kz@gmail.com>
2018-03-16 07:58:27 -0400

    some editions

--- code/lda/lda_YaleB.m
+++ code/lda/lda_YaleB.m
@@ -15,15 +15,15 @@ clear; close all;
 addpath('../utility');
 addpath('../YaleB', '../YaleB/data', '../YaleB/50Train', '../decision tree');
 %% before 
-[faces, labels] = loadYaleB();
-labels = labels - ones(size(labels,1));
-seed = 152039828;
-rng(seed); % for reproducibility
-load('2.mat'); % we load the indices to train and test sets
-train = {faces(trainIdx, :)', labels(trainIdx)};
-test = {faces(testIdx, :)', labels(testIdx)};
-k = 38;
-N_tr = size(trainIdx,1);
+% [faces, labels] = loadYaleB();
+% labels = labels - ones(size(labels,1));
+% seed = 152039828;
+% rng(seed); % for reproducibility
+% load('2.mat'); % we load the indices to train and test sets
+% train = {faces(trainIdx, :)', labels(trainIdx)};
+% test = {faces(testIdx, :)', labels(testIdx)};
+% k = 38;
+% N_tr = size(trainIdx,1);
 %%
 [train, test] = loadYaleB();
 train{1,2} = train{1,2} - 1;
@@ -31,11 +31,11 @@ test{1,2} = test{1,2} - 1;
 k = 38;
 N_tr = size(test{1,2},1);
 %% use PCA to see the results
-numFeatures = 50;
-[train, U, V] = pca_(train, numFeatures);
-train{1,1} = train{1,3};
-[test, U, V] = pca_(test, numFeatures);
-test{1,1} = test{1,3};
+% numFeatures = 50;
+% [train, U, V] = pca_(train, numFeatures);
+% train{1,1} = train{1,3};
+% [test, U, V] = pca_(test, numFeatures);
+% test{1,1} = test{1,3};
 %% Construct scatter matrices and calculate within-class and between class
 % covariance
 mu = mean(train{1,1}, 2);
@@ -64,25 +64,25 @@ a = [];
 % from here we can see that we only have 9 highest values as we expected
 %% this way is done using pca
 % We transform the training and testing data to a subspace
-transf_train = train{1,1}'* U(:,1:numFeatures);
-transf_test = test{1,1}'*U(:, 1:numFeatures);
-% calculting the multivariate parameters
-mu_each_class = zeros(numFeatures, k);
-cov_each_class = {};
-sum_cov = zeros(numFeatures,numFeatures);
-for i =0:k-1
-    ind = find(train{1,2} == i);
-    X = train{1,1}';
-    X = X(ind, :);
-    mu_each_class(:, i+1) = mean(X, 1)';
-    cov_each_class{1, i+1} = cov(X);
-    sum_cov = sum_cov + cov(X);
-end
-average_cov = sum_cov/k;
-
-cov_equal_each_class = {average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov};
-% to see how our model works
-[acc_test_comp acc_train_comp] = classify_comparison(k,5,mu_each_class, cov_equal_each_class, test{1,1}, test{1,2}, train{1,1}, train{1,2}); % 0.88 and 0.89 resp using just knn
+% transf_train = train{1,1}'* U(:,1:numFeatures);
+% transf_test = test{1,1}'*U(:, 1:numFeatures);
+% % calculting the multivariate parameters
+% mu_each_class = zeros(numFeatures, k);
+% cov_each_class = {};
+% sum_cov = zeros(numFeatures,numFeatures);
+% for i =0:k-1
+%     ind = find(train{1,2} == i);
+%     X = train{1,1}';
+%     X = X(ind, :);
+%     mu_each_class(:, i+1) = mean(X, 1)';
+%     cov_each_class{1, i+1} = cov(X);
+%     sum_cov = sum_cov + cov(X);
+% end
+% average_cov = sum_cov/k;
+% 
+% cov_equal_each_class = {average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov average_cov};
+% % to see how our model works
+% [acc_test_comp acc_train_comp] = classify_comparison(k,5,mu_each_class, cov_equal_each_class, test{1,1}, test{1,2}, train{1,1}, train{1,2}); % 0.88 and 0.89 resp using just knn
 
 %% We transform the training and testing data to a subspace
 transf_train = train{1,1}'* U(:,1:(k-1));
@@ -103,7 +103,7 @@ cov_equal_each_class = {average_cov average_cov average_cov average_cov average_
 % to see how our model works
 [acc_test_comp acc_train_comp] = classify_comparison(k,5,mu_each_class, cov_equal_each_class, transf_test', test{1,2}, transf_train', train{1,2}); % 0.88 and 0.89 resp using just knn
 
-
+%%
 [acc_test_5 acc_train_5] = classifyNN(k,5,mu_each_class, cov_equal_each_class, transf_test', test{1,2}, transf_train', train{1,2}); % 0.88 and 0.89 resp using just knn
 [acc_test_5_p acc_train_5_p] = classifyNN_pure(5,transf_test', transf_train', test{1,2}, train{1,2});
 
