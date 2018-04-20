1918896bb86adea8aaba683b1b8e5d73fb99fcfd
cody <kudi.kz@gmail.com>
2018-04-20 17:38:02 -0400

    a little change

--- code/lda/classify_comparison_same_cov.m
+++ code/lda/classify_comparison_same_cov.m
@@ -1,4 +1,4 @@
-function [acc_test acc_train] = classify_comparison_same_cov(total_classes,num_neighbors, mean_vector, covariance, test_data, test_label, train_data,  train_label)
+function [acc_test acc_train] = classify_comparison_same_cov(total_classes,num_neighbors, mean_vector, cov_each_class, covariance, test_data, test_label, train_data,  train_label)
 %
 % Description:  
 % Classify test data using Nearest Neighbor method withEuclidean distance
@@ -21,12 +21,13 @@ train_N = size(train_data,2);
 test_N = size(test_data, 2);
 counter = zeros(test_N, 1);
 C_inv = inv(covariance);
+
 % performance on test data
 parfor test_n = 1:test_N
     test_vector = test_data(:, test_n);
     f = zeros(total_classes,1);
     for i = 1:total_classes
-        f(i,1) = mean_vector(:,i)'*C_inv*test_vector - (1/2)*mean_vector(:,i)'*C_inv*mean_vector(:,i) + log(1/total_classes);
+        f(i,1) = mean_vector(:,i)'*C_inv*test_vector - (1/2)*mean_vector(:,i)'*C_inv*mean_vector(:,i) + log(1/total_classes);    
     end
     a = find(f == max(f)) - 1;
     if a == test_label(test_n)
